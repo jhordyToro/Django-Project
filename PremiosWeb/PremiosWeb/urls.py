af981105d0ae60ce99ed2bcdcd3a8a754b3ca181
Jhory Toro <jhordytoro49@gmail.com>
2022-04-06 17:53:38 -0500

    'settings_name'

--- PremiosWeb/PremiosWeb/urls.py
+++ PremiosWeb/PremiosWeb/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('polls', include('polls.url')),
+    path('polls', include('polls.urls')),
 ]
