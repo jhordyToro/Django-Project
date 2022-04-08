de52833e1960604dd12e1b3c2447056351f22cab
Jhory Toro <jhordytoro49@gmail.com>
2022-04-07 21:49:39 -0500

    'models'

--- PremiosWeb/polls/models.py
+++ PremiosWeb/polls/models.py
@@ -1,5 +1,6 @@
-from tkinter import CASCADE
+from datetime import timedelta
 from django.db import models
+from django.utils import timezone 
 
 # Create your models here.
 
@@ -7,7 +8,16 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return self.question_text
+
+    def time_antique(self):
+        return self.pub_date >= timezone.now() - timedelta(days=1)
+
 class Choise(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE) 
     choise_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choise_text
