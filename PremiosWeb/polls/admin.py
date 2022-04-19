414dfc07fbb796154c42346bb49d8718de97701f
Jhory Toro <jhordytoro49@gmail.com>
2022-04-18 23:12:27 -0500

    'main'

--- PremiosWeb/polls/admin.py
+++ PremiosWeb/polls/admin.py
@@ -1,6 +1,19 @@
+#polls/admin.py
 from django.contrib import admin
-from .models import Question, Choise
+from .models import Choise, Question
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choise)
+#Creando un TabularInLine desde el modelo Choise
+class ChoiseInline(admin.StackedInline):
+    model = Choise
+    extra = 3
+
+#Creando un ModelAdmin desde el modelo Question
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date', 'question_text']
+    inlines = [ChoiseInline]
+    list_display = ('question_text', 'pub_date', 'time_antique' )
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
